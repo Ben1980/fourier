638a0213e40c7304ad490b78aa0811f87bf50489
Benjamin Mahr <Ben1980@users.noreply.github.com>
2020-01-17 21:28:48 +0100

    Initial commit

---
+++ lib/lib.cpp
@@ -0,0 +1,7 @@
+#include "lib.h"
+#include <iostream>
+
+Library::PrintHelloWorld()
+{
+    std::cout << "Hello World!" << '\n';
+}
---
+++ lib/lib.h
@@ -0,0 +1,8 @@
+#ifndef LIB_H
+#define LIB_H
+
+namespace Library {
+  PrintHelloWorld();
+}
+
+#endif
