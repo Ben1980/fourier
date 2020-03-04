fba4e5e0887b3926cc1ca43403857caa16f72d70
Benjamin Mahr <ben.mahr@gmail.com>
2020-03-04 21:57:35 +0100

    Starting a data parser

--- dft.h
+++ dft.h
@@ -1,11 +1,12 @@
 #ifndef FOURIER_H
 #define FOURIER_H
 
-
+#include "helper.h"
 
 TEST_SUITE("Fourier test suite") {
     TEST_CASE ("DFT test") {
-        
+        auto data = TestDataHelper::ParseDataFile();
+
         CHECK(false);
         
     }
---
+++ helper.h
@@ -0,0 +1,15 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+#include <vector>
+
+namespace TestDataHelper 
+{
+    std::vector<std::pair<double, double>> ParseDataFile()
+    {
+        return {};
+    }
+
+};
+
+#endif
