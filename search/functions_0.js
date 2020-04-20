5246d9d7e132b21e5b06601124eabf9fc7ee9549
Ben1980 <Ben1980@users.noreply.github.com>
2020-04-20 19:22:04 +0000

    [ci skip] documentation of helper library added 151fafd6ae82dbd677838cafb9f15669e7a7be42

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['equal_63',['Equal',['../namespaceFourier.html#a550d31c1224f6fa3b7abf824f2c831e7',1,'Fourier::Equal(const Coefficients &amp;expected, const Coefficients &amp;result)'],['../namespaceFourier.html#ad11de430ec2930a326e82f0a3278b219',1,'Fourier::Equal(double a, double b, double maximumDelta=1e-15)']]]
+  ['equal_63',['Equal',['../namespaceFourier.html#a550d31c1224f6fa3b7abf824f2c831e7',1,'Fourier::Equal(const Coefficients &amp;expected, const Coefficients &amp;result)'],['../namespaceFourier.html#af8429ecf21d5a55601f55ea498b99f59',1,'Fourier::Equal(double expected, double result, double maximumDelta=1e-15)']]]
 ];
